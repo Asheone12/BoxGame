0b4b1e6ef45979e07b5e6d65f1be20c16226d05a
xujianglong <jianglong.xu@tiho-mobi.com>
2024-03-25 17:15:21 +0800

    首次提交，实现基本功能

---
+++ app/src/main/java/com/muen/boxgame/LevelInitialData.java
@@ -0,0 +1,21 @@
+package com.muen.boxgame;
+
+
+public class LevelInitialData {
+    int mRowNum;
+    int mColumnNum;
+
+    String [] mInitialState;
+
+    public LevelInitialData(int rowNum, int columnNum){
+        mRowNum= rowNum;
+        mColumnNum = columnNum;
+        mInitialState = new String[rowNum];
+    }
+
+    public LevelInitialData(int rowNum, int columnNum, String[] initialState){
+        mRowNum = rowNum;
+        mColumnNum = columnNum;
+        mInitialState = initialState;
+    }
+}
